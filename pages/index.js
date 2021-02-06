432a89729bb605ce0d88ef7d5c1f480f21907a1e
Marco Chan <marco.chan30@gmail.com>
2021-02-05 22:10:23 -0500

    Updated with available images and personal info

--- pages/index.js
+++ pages/index.js
@@ -81,11 +81,11 @@ export default function Home() {
 
 					<span className={styles.teamCard}>
 						<div className={styles.teamImageWrapper}>
-							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
+							<img src="/headshots/aitik.png" className={styles.headshot}></img>
 						</div>
 						<h3>Aitik Gupta</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>India</p>
+						<p className={styles.university}>IIIT Gwalior</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -98,11 +98,11 @@ export default function Home() {
 
 					<span className={styles.teamCard}>
 						<div className={styles.teamImageWrapper}>
-							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
+							<img src="/headshots/anjali.jpg" className={styles.headshot}></img>
 						</div>
 						<h3>Anjali Soni</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>India</p>
+						<p className={styles.university}>IIT Guwahati</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -115,11 +115,11 @@ export default function Home() {
 
 					<span className={styles.teamCard}>
 						<div className={styles.teamImageWrapper}>
-							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
+							<img src="/headshots/bodisha.jpeg" className={styles.headshot}></img>
 						</div>
 						<h3>Bodisha Thomas</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>India</p>
+						<p className={styles.university}>APJ Abdul Kalam University</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -149,11 +149,11 @@ export default function Home() {
 
 					<span className={styles.teamCard}>
 						<div className={styles.teamImageWrapper}>
-							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
+							<img src="/headshots/dean.jpg" className={styles.headshot}></img>
 						</div>
 						<h3>Dean Gladish</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>United States</p>
+						<p className={styles.university}>Carleton College</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -166,11 +166,11 @@ export default function Home() {
 
 					<span className={styles.teamCard}>
 						<div className={styles.teamImageWrapper}>
-							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
+							<img src="/headshots/deepak.jpg" className={styles.headshot}></img>
 						</div>
 						<h3>Deepak Agrawal</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>India</p>
+						<p className={styles.university}>Savitribai Phule Pune Uni.</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -203,8 +203,8 @@ export default function Home() {
 							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
 						</div>
 						<h3>Prashi Doval</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>India</p>
+						<p className={styles.university}>IGDTUW</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -217,11 +217,11 @@ export default function Home() {
 
 					<span className={styles.teamCard}>
 						<div className={styles.teamImageWrapper}>
-							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
+							<img src="/headshots/rashi.jpg" className={styles.headshot}></img>
 						</div>
 						<h3>Rashi Sharma</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>India</p>
+						<p className={styles.university}>NIT Kurukshetra</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -234,11 +234,11 @@ export default function Home() {
 
 					<span className={styles.teamCard}>
 						<div className={styles.teamImageWrapper}>
-							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
+							<img src="/headshots/samuel.jpeg" className={styles.headshot}></img>
 						</div>
 						<h3>Samuel Yuan</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>United States</p>
+						<p className={styles.university}>High School</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -254,8 +254,8 @@ export default function Home() {
 							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
 						</div>
 						<h3>Sena Yevenyo</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>United States</p>
+						<p className={styles.university}>Lafayette College</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -268,11 +268,11 @@ export default function Home() {
 
 					<span className={styles.teamCard}>
 						<div className={styles.teamImageWrapper}>
-							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
+							<img src="/headshots/shrill.jpg" className={styles.headshot}></img>
 						</div>
 						<h3>Shrill Shrestha</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>United States</p>
+						<p className={styles.university}>University of Louisiana</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -305,8 +305,8 @@ export default function Home() {
 							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
 						</div>
 						<h3>Sumi Kolli</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>United States</p>
+						<p className={styles.university}>USC</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -322,8 +322,8 @@ export default function Home() {
 							<img src="/headshots/marco.jpg" className={styles.headshot}></img>
 						</div>
 						<h3>Yvon Manzi</h3>
-						<p>Canada</p>
-						<p className={styles.university}>University of Pennsylvania</p>
+						<p>United States</p>
+						<p className={styles.university}>Columbia University</p>
 						<a href="https://www.github.com" target="_blank">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" className={styles.linkIcon} viewBox="0 0 16 16">
 								<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
